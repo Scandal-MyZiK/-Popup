78d6172f2e96ca3659671504370e7787516e1151
Your Name <yang.lin.99@bk.ru>
2023-07-14 18:01:17 +0300

    final

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.querySelector('.menu-btn');
-    menuBtn.addEventListener('click', () => {
-        const headerList = document.querySelector('.header-menu__list');
-        headerList.classList.toggle('active');
-        menuBtn.classList.toggle('active');
+    document.querySelector('#open-modal-btn').addEventListener('click', function(){
+        document.querySelector('#my-modal').classList.add('active');
     });
+    document.querySelector('#close-my-modal-btn').addEventListener('click', function(){
+        document.querySelector('#my-modal').classList.remove('active');
+    });
+
+    //Закрыть попап с помощью Escape
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            document.querySelector('#my-modal').classList.remove('active');
+        }
+    })
+
+    //Закрыть попап кликом вне его
+    document.querySelector('#my-modal .pop_up__body').addEventListener('click', event => {
+        event._isClickWithInModal = true;
+    });
+
+    document.querySelector('#my-modal').addEventListener('click', event => {
+        if (event._isClickWithInModal) return;
+        event.currentTarget.classList.remove('active');
+    })
 });
